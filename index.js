dbd81a1aa01a95a54d130eb2de8e5a8affbab470
tsutsu <tsutsu@example.com>
2020-10-05 02:40:10 +0900

    CSRF対策を追加

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const csrf = require('csurf');
 const post = require('./routs/post');
 
 const app = express();
@@ -8,10 +11,19 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+app.use(cookieParser());
+app.use(expressSession({ secret: 'my-ssecret-key' }));
+app.use(csrf());
+app.use((req, res, next) => {
+    res.locals.csrftoken = req.csrfToken();
+    next();
+});
+
+app.use(logger('dev'));
 
 app.get('/', post.index);
 app.get('/posts/:id([0-9]+)', post.show);
